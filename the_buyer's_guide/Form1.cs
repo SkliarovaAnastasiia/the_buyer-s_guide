d8e87cd071a695109b81121ab1c2859ce1bd988b
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-04-29 16:48:43 +0300

    add buttons

--- the_buyer's_guide/Form1.cs
+++ the_buyer's_guide/Form1.cs
@@ -1,10 +1,17 @@
 namespace the_buyer_s_guide
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SearchForm searchForm = new SearchForm();
+            searchForm.Show();
         }
     }
 }
